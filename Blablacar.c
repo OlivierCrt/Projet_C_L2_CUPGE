7d9eb31168505c8eaefa73b7b509964084cd344c
R0Mei <113097301+R0Mei@users.noreply.github.com>
2023-04-09 15:54:27 +0200

    Update Blablacar.c

--- Blablacar.c
+++ Blablacar.c
@@ -113,10 +113,10 @@ int afficher_menu() {
     printf("Pour parcourir notre application veuillez saisir un nombre de 1 à 9 pour accéder a l'espace\n\n");
     printf("1. Gestions des Villes");
     printf("2. Liste des Conducteurs");
-    printf("3. ");
-    printf("4.");
-    printf("5.");
-    printf("6.");
+    printf("3. Gérer les Comptes");
+    printf("4. Demande Trajets");
+    printf("5. Offres Trajets");
+    printf("6. Gérer les Comptes Conduteurs");
     printf("7.");
     printf("8.");
     printf("9. Quitter");
