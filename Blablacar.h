63cb96d89d7b414410027f7fa42cc73e09147646
Solal <113097238+solaldl@users.noreply.github.com>
2023-04-09 17:30:33 +0200

    Update Blablacar.h

--- Blablacar.h
+++ Blablacar.h
@@ -1,12 +1,15 @@
+#ifndef BLABLACAR_H
+#define BLABLACAR_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-
 typedef struct s_date* date;
-typedef struct s_tronçon* Troncon;
+typedef struct s_troncon* Troncon;
 typedef struct s_trajet* Trajet;
-typedef struct s_ville* Ville;
+typedef struct s_ville* Lville;
 typedef struct s_conducteur* Conducteur;
 typedef struct s_client* Client ;
 
+#ifndef
