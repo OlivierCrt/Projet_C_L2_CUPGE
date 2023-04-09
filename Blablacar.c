8a3a273709beb8d5aa9264504fbb58c02740196c
R0Mei <113097301+R0Mei@users.noreply.github.com>
2023-04-09 16:00:46 +0200

    Update Blablacar.c

--- Blablacar.c
+++ Blablacar.c
@@ -120,6 +120,7 @@ int afficher_menu() {
     printf("7. Choisir un trajet (espace Client)");
     printf("8. Modifier / Annuler un trajet (espace Client)");
     printf("9. Quitter");
+    scanf( "Sélectionnez un paramètre : ", choix );
     return choix_utilisateur;
 }
 
