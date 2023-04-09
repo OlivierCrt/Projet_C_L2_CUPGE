6c9dcf3f912018381214f1977521963d6db7e1fd
OlivierCrt <113097991+OlivierCrt@users.noreply.github.com>
2023-04-09 16:57:48 +0200

    Update Blablacar.h

--- Blablacar.h
+++ Blablacar.h
@@ -3,3 +3,10 @@
 #include <stdbool.h>
 
 
+typedef struct s_date* date;
+typedef struct s_tronçon* Troncon;
+typedef struct s_trajet* Trajet;
+typedef struct s_ville* Ville;
+typedef struct s_conducteur* Conducteur;
+typedef struct s_client* Client ;
+
