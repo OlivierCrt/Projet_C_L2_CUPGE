ac56c360a8ce960c91f38a12f363597ca6d40425
Solal <113097238+solaldl@users.noreply.github.com>
2023-05-03 17:18:10 +0200

    Update Romain_Olivier_Solal.c

--- Romain_Olivier_Solal.c
+++ Romain_Olivier_Solal.c
@@ -20,27 +20,22 @@ typedef struct s_trajet{
 }Trajet;
 
 typedef struct s_ville{
-  char* nom
+  char* nom;
 }Ville;
 
 typedef struct s_conducteur{
-  char* nom ;
-  char* prenom;
+  char* id;
   int mdp;
 }Conducteur;
 
 typedef struct s_client{
-  char* nom ;
-  char*prenom;
+  char* id;
 }Client;
 
 typedef struct s_admin{
-  char* nom;
-  char* prenom;
+  char* id;
   int mdp;
 }Admin;
-
-
 
 //0-
 void login(Conducteur* conducteurs, Client* clients, Admin admin ); 
@@ -67,4 +62,68 @@ void reserver_trajet(Trajet* trajets, Ville* villes, Conducteur* conducteurs, Cl
 //9-
 void exit();
 
-#endif
+
+//0- login
+void login(Conducteur* conducteurs, Client* clients, Admin admin) {
+    int id;
+    int mdp;
+    int choix;
+    printf("Voulez-vous vous connecter en tant que : \n");
+    printf("1- Conducteur\n");
+    printf("2- Client\n");
+    printf("3- Administrateur\n");
+    printf("4- Quitter le programme\n");
+    scanf("%d", &choix);
+    switch (choix) {
+    case 1:
+        printf("Identifiant : ");
+        scanf("%d", &id);
+        printf("Mot de passe : ");
+        scanf("%d", &mdp);
+        for (int i = 0; i < NB_CONDUCTEURS; i++) {
+            if ((conducteurs[i].id == id) && (conducteurs[i].mdp == mdp)) {
+                printf("Bienvenue %s %s !\n", conducteurs[i].id);
+                menu_conducteur(conducteurs[i], clients, trajets, villes);
+                return;
+            }
+        }
+        printf("Identifiant ou mot de passe incorrect\n");
+        break;
+    case 2:
+        printf("Identifiant : ");
+        scanf("%d", &id);
+        printf("Mot de passe : ");
+        scanf("%d", &mdp);
+        for (int i = 0; i < NB_CLIENTS; i++) {
+            if ((clients[i].id == id) && (clients[i].mdp == mdp)) {
+                printf("Bienvenue %s %s !\n", clients[i].id);
+                menu_client(clients[i], trajets, villes, conducteurs);
+                return;
+            }
+        }
+        printf("Identifiant ou mot de passe incorrect\n");
+        break;
+    case 3:
+        printf("Identifiant : ");
+        scanf("%d", &id);
+        printf("Mot de passe : ");
+        scanf("%d", &mdp);
+        if ((admin.id == id) && (admin.mdp == mdp))) {
+            printf("Bienvenue %s %s !\n", admin.id);
+            menu_admin(conducteurs, clients, trajets, villes);
+            return;
+        }
+        printf("Identifiant ou mot de passe incorrect\n");
+        break;
+    case 4:
+        printf("Au revoir !\n");
+        exit(0);
+    default:
+        printf("Choix invalide\n");
+    }
+    login(conducteurs, clients, admin);
+}
+
+
+
+
