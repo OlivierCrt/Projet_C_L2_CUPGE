0de904dd4374ef1bfd4b9e052ec6a372d528351f
Solal <113097238+solaldl@users.noreply.github.com>
2023-05-08 15:09:23 +0200

    Update Romain_Olivier_Solal.c

--- Romain_Olivier_Solal.c
+++ Romain_Olivier_Solal.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <openssl/evp.h> //cryptage
-#include "Romain_Olivier_Solal.h" 
-#include <jansson.h>
+//#include <openssl/evp.h> //cryptage
+//#include "Romain_Olivier_Solal.h" 
+//#include <jansson.h>
 
 typedef struct s_date {
     int annee;
@@ -13,12 +13,10 @@ typedef struct s_date {
     int minute;
 }Date;
 
-typedef struct s_trajet {
-    Ville* villes[];
-    Date* dates;
-    Conducteur conducteur;
-    Client** clients;
-}Trajet;
+typedef struct s_admin {
+    char* id;
+    int mdp;
+}Admin;
 
 typedef struct s_ville {
     char* nom;
@@ -31,58 +29,32 @@ typedef struct s_conducteur {
 
 typedef struct s_client {
     char* id;
-    int mdp
+    int mdp;
 }Client;
 
-typedef struct s_admin {
-    char* id;
-    int mdp;
-}Admin;
+typedef struct s_trajet {
+    Ville* villes;
+    Date* dates;
+    Conducteur conducteur;
+    Client** clients;
+    int Nb_etapes;
+}Trajet;
 
-Admin admin;
-Conducteur* conducteurs;
-Client* clients;
-Trajet* trajets;
-Ville* villes*
+
+Admin admin=NULL;
+Conducteur* conducteurs=NULL;
+Client* clients=NULL;
+Trajet* trajets=NULL;
+Ville* villes = NULL;
 
 int NB_conducteurs = 0;
 int NB_clients = 0;
 int NB_trajets = 0;
 int NB_villes = 0;
 
-
-//----------------DEBUT DU CODE-----------------
-
-void login(Conducteur* conducteurs, Client* clients, Admin admin) {
-    int choix;
-    printf("Voulez-vous vous connecter en tant que : \n");
-    printf("1- Administrateur\n");
-    printf("2- Conducteur\n");
-    printf("3- CLient\n");
-    printf("4- Quitter le programme\n");
-    scanf("%d", &choix);
-    if (choix == 1) {
-        login_admin(admin);
-    }
-    if (choix == 2) {
-        login_conducteur(conducteurs);
-    }
-    if (choix == 3{
-        login_client(clients);
-    }
-    if (choix == 4){
-        printf("Au revoir !\n");
-        exit(0);
-    }
-    else{ 
-        printf("Choix invalide\n");
-        login(conducteurs, clients, admin);
-    }
-}
-
 //-------------------LOGIN---------------------------
 
-void login_admin(admin) {
+void login_admin(Admin admin) {
     int id;
     int mdp;
     printf("Identifiant : ");
@@ -97,7 +69,7 @@ void login_admin(admin) {
     login_admin(admin);
 }
 
-void login_conducteur(Conducteur* conducteurs){
+void login_conducteur(Conducteur* conducteurs, int NB_conducteurs){
     int id;
     int mdp;
     printf("Identifiant : ");
@@ -114,7 +86,7 @@ void login_conducteur(Conducteur* conducteurs){
     login_conducteur(conducteurs);
 }
 
-    void login_client(clients) {
+    void login_client(Client* clients, int NB_clients) {
         int id;
         int mdp;
         printf("Identifiant : ");
@@ -178,22 +150,23 @@ void menu_admin(Conducteur* conducteurs, Client* clients, Trajet* trajets, Ville
 }
 
 //1
-void add_v(Ville* villes) {
+void add_v(Ville* villes, int NB_villes) {
     printf("Nom de la ville à ajouter : ");
     char nom[50];
-    scanf("%s", nom);
+    scanf("%s", &nom);
     Ville ville = { nom };
     villes[NB_villes] = ville;
     printf("La ville %s a bien été ajoutée !\n", nom);
-    NB_VILLES++;
+    NB_villes++;
     return;
 }
+
 
 //2
 void edit_v(Ville* villes, Ville ville) {
     printf("Nom de la ville à modifier : ");
     char nom[50];
-    scanf("%s", nom);
+    scanf("%s", &nom);
     for (int i = 0; i < NB_villes; i++) {
         if (strcmp(villes[i]->nom, nom) == 0) {
             printf("Nouveau nom de la ville : ");
@@ -210,7 +183,7 @@ void edit_v(Ville* villes, Ville ville) {
 void sup_v(Ville* villes, Ville ville) {
     printf("Nom de la ville à supprimer : ");
     char nom[50];
-    scanf("%s", nom);
+    scanf("%s", &nom);
     for (int i = 0; i < NB_villes; i++) {
         if (strcmp(villes[i]->nom, nom) == 0) {
             for (int j = i; j < NB_villes - 1; j++) {
@@ -241,7 +214,7 @@ void import_json(Conducteur* Concducteur, Client* Clients, Trajet* trajets, Vill
 void admin_mdp_conducteur(Conducteur* conducteurs) {
     printf("ID du conducteur dont vous souhaitez modifier le mot de passe : ");
     char id[50];
-    scanf("%s", id);
+    scanf("%s", &id);
     for (int i = 0; i < NB_conducteurs; i++) {
         if (strcmp(conducteurs[i]->id, id) == 0) {
             printf("Nouveau Mot de passe : ");
@@ -258,11 +231,11 @@ void admin_mdp_conducteur(Conducteur* conducteurs) {
 void admin_mdp_client(Client* clients){
     printf("ID du client dont vous souhaitez modifier le mot de passe : ");
     char id[50];
-    scanf("%s", id);
+    scanf("%s", &id);
     for (int i = 0; i < NB_clients; i++) {
         if (strcmp(clients[i]->id, id) == 0) {
             printf("Nouveau Mot de passe : ");
-            scanf("%s", clients[i]->mdp);
+            scanf("%s", &clients[i]->mdp);
             printf("Le mot de passe %s a été modifiée avec succès !\n", clients[i]->id);
             return;
         }
@@ -301,13 +274,58 @@ void menu_conducteur(Conducteur conducteur, Client* clients, Trajet* trajets, Vi
 }
 
 //1
-void creer_trajet(Conducteur conducteur, Trajet* trajets, Ville* villes);
+void creer_trajet(Conducteur conducteur, Trajet* trajets, Ville* villes) {
+    Trajet* new_trajet = malloc(sizeof(Trajet));
+    int nb;
+    printf("Entrez le nombre de ville dans votre trajet")
+    scanf("%d", &nb);
+    for (i = 1; i <= nb+1; i++) {
+        printf("Entrez la ville %d de votre trajet", i)
+    }
+    Ville* traj_villes = malloc(sizeof(;
+
+    printf("Entrez dans l'ordre toutes les villes par lesquels vous allez passer.");
+
+    scanf("%s", &id);
+    new_trajet->villes = ;
+    new_trajet->dates = ;
+    new_trajet->conducteur = conducteur;
+    new_trajet->clients;
+
+
+    typedef struct s_trajet {
+        Ville* villes;
+        Date* dates;
+        Conducteur conducteur;
+        Client** clients;
+    }Trajet;
+}
+
 
 //2
-void afficher_trajets(Trajet* trajets, Conducteur conducteur);
+void afficher_trajets(Trajet* trajets, int Nb_etapes, Conducteur conducteur) {
+    for (int i; i < NB_conducteurs; i++) {
+        if trajets[i]->conducteur = conducteur{
+            printf("TRAJET DU %d / %d  %d", trajet,dates[i]->jour, trajet[i]->dates[i]->mois, trajet[i]->dates[i]->annee);
+            for (int j = 0; int j < Nb_etapes; j++) {
+                char* a = trajet[i]->clients[i][0]; //changer le format de la liste de client
+                char* b = trajet[i]->clients[i][1];
+                printf("%d - Ville : %s ; Départ à : %d h %d ; Clients enregistrés sur ce tronçon : %s , %s", trajet[i]->villes[j], trajet[i]->dates[i]->heure, trajet[i]->dates[i]->minute, a, b);
+            }
+        }
+    }
+    printf("Tous vos trajets ont été affichés");
+    return;
+}
 
 //3
-void changer_logs_conducteur(Conducteur conducteur);
+void changer_logs_conducteur(Conducteur conducteur) {
+    printf("Entrez votre nouveau mot de passe");
+    char* new_mdp;
+    scanf("%s", &new_mdp);
+    conducteur->mdp = new_mdp;
+    return;
+}
 
 
 //-----------------CLIENT----------------
@@ -323,7 +341,7 @@ void menu_client(Conducteur* conducteur, Client* clients, Trajet* trajets, Ville
         creer_trajet(conducteur, trajets, villes);
     }
     if (choix == 2) {
-        afficher_trajets(trajets, conducteur);
+        afficher_trajets(trajets, conducteur)²
     }
     if (choix == 3{
         changer_logs_conducteur(conducteur);
@@ -334,13 +352,79 @@ void menu_client(Conducteur* conducteur, Client* clients, Trajet* trajets, Ville
     }
     else {
         printf("Choix invalide\n");
-        menu_client(conducteurs, client, trajets, villes);
+            menu_client(conducteurs, client, trajets, villes);
     }
 }
-    
+
 //1
-void chercher_trajet(Trajet* trajets, Ville* villes, Conducteur* conducteurs); // osef des conducteur 
+void chercher_trajet(Trajet* trajets, int NB_trajets, Ville* villes, Conducteur* conducteurs) { // osef des conducteur 
+    printf("Rentrez l'année de votre trajet");
+    int an;
+    scanf("%d", &an);
+    printf("Rentrez le mois de votre trajet");
+    int mo;
+    scanf("%d", &mo);
+    printf("Rentrez le jour de votre trajet");
+    int jo;
+    scanf("%d", &jo);
+    printf("Rentrez votre ville de départ");
+    char* dep;
+    scanf("%s", &dep);
+    printf("Rentrez votre ville d'arrivé");
+    char* arr;
+    scanf("%s", &arr);
+    for (int i = 0; i < NB_trajets; i++) {
+        if ((trajets[i]->date[0]->annee == an) && (trajets[i]->date[0]->mois == mo) && (trajets[i]->date[0]->jour == jo)) {
+            for (int j = 0; j < trajets[i]->trajets[i]->Nb_etapes; j++) {
+                if (trajets[i]->villes[j] == dep) {
+                    int nb_dep = j;
+                    for (int j = nb; j < trajets[i]->trajets[i]->Nb_etapes; j++) {
+                        if (trajets[i]->villes[j] == arr) {
+                            int nb_arr = j; 
+                            //cherche si le traj existe mais pas encore si les places sont libres
+                            printf("Trajet %d : départ à %d h &d", &i, &trajets[i]->date[nb_dep]->heure, &trajets[i]->date[nb_dep]->minute);
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    printf("Il n'y a pas de trajets satisfaisants vos conditions"); 
+    chercher_trajet(trajets, NB_trajets, villes, conducteurs);
+}
 
 //2
-void reserver_trajet(Trajet* trajets, Ville* villes, Conducteur* conducteurs, Client client); //idem 
+void reserver_trajet(Trajet* trajets, Ville* villes, Conducteur* conducteurs, Client client) { //conducteur osef 
 
+}
+
+//----------------MAIN---------------------
+
+    void main(Conducteur* conducteurs, Client* clients, Admin admin) {
+        int choix;
+        printf("Voulez-vous vous connecter en tant que : \n");
+        printf("1- Administrateur\n");
+        printf("2- Conducteur\n");
+        printf("3- CLient\n");
+        printf("4- Quitter le programme\n");
+        scanf("%d", &choix);
+        if (choix == 1) {
+            login_admin(admin);
+        }
+        else if (choix == 2) {
+            login_conducteur(conducteurs);
+        }
+        else if (choix == 3{
+            login_client(clients);
+        }
+        else if (choix == 4) {
+            printf("Au revoir !\n");
+            exit(0);
+        }
+        else {
+            printf("Choix invalide\n");
+            main(conducteurs, clients, admin);
+        }
+    }
