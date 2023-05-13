0365010603eb9af143f0ac634ee4b019ad7bea5e
OlivierCrt <113097991+OlivierCrt@users.noreply.github.com>
2023-05-13 16:25:35 +0200

    Update Blablacar.h

--- Blablacar.h
+++ Blablacar.h
@@ -29,9 +29,31 @@ int taille_liste_json(json_t* dataJson , const char * motclé);
 
 //----------tous---------
 void login(Conducteur* conducteurs, Client* clients, Admin admin);
-void menu_admin(const char** villes, int *NB_villes);
-void login_admin(Admin admin, const char** villes, int *NB_villes);
+void menu_admin(const char** villes, int *NB_villes  ,   const char ** conds , int* NB_conds, const char** mdpconds);
+void login_admin(Admin admin, const char** villes, int *NB_villes , const char** conds , int* NB_conds, const char** mdpconds);
+void login_conducteur(Admin admin, const char** villes, int *NB_villes , const char** conds , int* NB_conds, const char** mdpconds);
+
+int date_valide(int annee, int mois, int jour, int heure, int minute);
+
+int ville_valide(const char** villes, int *NB_villes, char nom[50]);
 
 void sup_v(const char** villes, int *NB_villes);
 void add_v(const char** villes, int* NB_villes);
 
+void edit_v(const char** villes, int* NB_villes);
+
+
+void sup_c(const char** villes, int *NB_villes);
+void add_c(const char** villes, int* NB_villes);
+
+void edit_c(const char** villes, int* NB_villes);
+
+
+
+void sup_MDPc(const char** villes, int *NB_villes);
+void add_MDPc(const char** villes, int* NB_villes);
+void edit_MDPc(const char** villes, int* NB_villes);
+
+void menu_modif_cond(const char** conds, int* NB_conds   ,   const char** villes, int *NB_villes, const char** mdpconds);
+
+void creer_trajet(const char* conducteur, const char** villes, int * NB_villes);
